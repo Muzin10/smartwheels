9f78ee0290bf6ada7ea2296496a546453d073236
Muhsin <muhsin30c@gmail.com>
2025-06-05 10:43:07 +0530

    colorconstant added

---
+++ lib/customwidgetss/textwidget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class TextSW extends StatelessWidget {
+  final String text;
+  final double? fontSize;
+  final Color color;
+  final FontWeight fontWeight;
+
+  const TextSW({
+    required this.text,
+    this.fontSize,
+    this.color = Colors.white,
+    this.fontWeight = FontWeight.bold,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize??width*0.03,
+        color: color,
+        fontWeight: fontWeight,
+      ),
+    );
+  }
+}
+
