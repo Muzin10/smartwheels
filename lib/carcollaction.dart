2e4428eb4499f6a4aab2b38ff246051ba088efbe
Muhsin <muhsin30c@gmail.com>
2025-06-05 11:52:31 +0530

    colorconstant added

---
+++ lib/carcollaction.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CarCollactionPage extends StatefulWidget {
+  const CarCollactionPage({super.key});
+
+  @override
+  State<CarCollactionPage> createState() => _CarCollactionPageState();
+}
+
+class _CarCollactionPageState extends State<CarCollactionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
