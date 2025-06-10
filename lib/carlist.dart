e7e4ea3522bfec704296baf46d0978ea65c1c094
Muhsin <muhsin30c@gmail.com>
2025-06-10 11:32:36 +0530

    new page added

---
+++ lib/carlist.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CarListPage extends StatefulWidget {
+  const CarListPage({super.key});
+
+  @override
+  State<CarListPage> createState() => _CarListPageState();
+}
+
+class _CarListPageState extends State<CarListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
