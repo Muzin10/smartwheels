12862b9ac8832b13b319a1524533c13cad27e4a5
Shahilkt <shahilkt02@gmail.com>
2025-06-10 10:31:30 +0530

    car collection page addded

--- lib/carcollaction.dart
+++ lib/carcollaction.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:smartwheels/constants/colorconstants/colorconstants.dart';
+
+import 'main.dart';
 
 class CarCollactionPage extends StatefulWidget {
   const CarCollactionPage({super.key});
@@ -11,7 +14,62 @@ class _CarCollactionPageState extends State<CarCollactionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
+       appBar: AppBar(
+         title: Text("CAR COLLACTION",
+         style: TextStyle(
+           color: ColorConstant.whiteColor
+         ),),
+         centerTitle: true,
+       ),
+      body: Padding(
+        padding:EdgeInsets.all(width*0.03),
+        child:Column(
+          children: [
+            Expanded(
+              child: GridView.builder(
+                itemCount: 8,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        height: height*0.4,
+                        width: width*0.8,
+                        decoration: BoxDecoration(
+                        image: DecorationImage(image: AssetImage("assets/images/blackcar.jpg")),
+                            color: ColorConstant.blackColor,
+                            borderRadius: BorderRadius.circular(width*0.03)
+                        ),
+                        child: Positioned(
+                          bottom: 10,
+                          left: 10,
+                          child: Text("MCLAREN GT",
 
+                            style: TextStyle(
+
+                              color: ColorConstant.whiteColor,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  );
+                },
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount:1,
+                    mainAxisSpacing: width*0.03,
+                    crossAxisSpacing: width*0.03,
+                    childAspectRatio: 1
+                ),
+
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'constants/colorconstants/colorconstants.dart';
+import 'loginpage.dart';
+
 class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({super.key});
 
@@ -8,10 +11,27 @@ class SplashScreenPage extends StatefulWidget {
 }
 
 class _SplashScreenPageState extends State<SplashScreenPage> {
+
+  void initState() {
+    // TODO: implement initState
+    Future.delayed(Duration(
+        seconds: 5
+    )).then((value) => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginpagePage(),)), );
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorConstant.blackColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(image: AssetImage("assetName"))
+          ],
 
+        ),
+    )
     );
   }
 }
